906227900bb366bbc9efded0daa114a895d0fe03
Jan Hadas <jan.hadas@i.cz>
2016-04-27 08:17:48 +0200

    configuration for database H2

---
+++ src/main/java/cz/i/db/H2Constants.java
@@ -0,0 +1,11 @@
+package cz.i.db;
+
+/**
+ * @author jan.hadas@i.cz
+ */
+public interface H2Constants {
+  public static final String DB_DRIVER = "org.h2.Driver";
+  public static final String DB_CONNECTION_STRING = "jdbc:h2:~/test";
+  public static final String DB_USER = "";
+  public static final String DB_PASSWORD = "";
+}
